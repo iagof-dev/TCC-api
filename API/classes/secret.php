3074732e85d6ade6f131d28a2f409fccea48c032
Iago F. <n3rdydzn@hotmail.com>
2024-03-29 16:43:51 -0300

    fix: database & api

--- API/classes/secret.php
+++ API/classes/secret.php
@@ -2,10 +2,10 @@
 
 class config{
   private $informations = [
-    "db_ip" => "127.0.0.1", //colocar "mysql" de volta dps
-    "db_port" => "3307",
+    "db_ip" => "mysql", //colocar "mysql" de volta dps
+    "db_port" => "3306",
     "db_user" => "root",
-    "db_pass" => "etecjau"
+    "db_pass" => "132490Kj@br="
   ];
 
   function get(){
--- db.sql
+++ db.sql
@@ -71,3 +71,19 @@ nome mediumtext not null,
 numero varchar(20) not null,
 curso tinytext not null
 );
+
+
+insert into alunos values (default, 'Iago Fragnan', 3, 'Desenvolvimento de Sistemas', '11'),
+(default, 'Arthur Santana', 3, 'Desenvolvimento de Sistemas', '75'),
+(default, 'Alexsandro Rodrighero', 3, 'Mecatronica', '14');
+
+insert into generos(tipo) values ('Romance'), ('Ação'), ('Didático'), ('Drama'), ('Novela'), ('Conto'), ('Ficção'), ('Biografia'), ('Poesia'), ('Aventura'), ('História em Quadrinhos'), ('Literatura Infantil'), ('Terror'), ('Artigo acadêmico'), ('Artigo científico'), ('Monografia'), ('Trabalho de Conclusão de Curso'), ('Tese de Doutorado'), ('Dissertação de Mestrado');
+
+
+INSERT INTO livros (codigo, titulo, autor, capa, volumes, sinopse) VALUES 
+(1001, 'Dom Casmurro', 'Machado de Assis', 'A', 1, 'Dom Casmurro é uma das obras mais conhecidas do autor brasileiro Machado de Assis. O romance é contado em primeira pessoa por Bento Santiago, o narrador, que se autodenomina Dom Casmurro.'),
+(1002, 'O Pequeno Príncipe', 'Antoine de Saint-Exupéry', 'B', 1, 'O Pequeno Príncipe é uma fábula sobre o amor e a solidão, na qual um piloto se encontra perdido no deserto onde conhece um pequeno príncipe de outro planeta.'),
+(1003, 'A Revolução dos Bichos', 'George Orwell', 'C', 1, 'A Revolução dos Bichos é uma fábula satírica que visa criticar o totalitarismo, contando a história de animais de uma fazenda que se revoltam contra seu dono.'),
+(1004, '1984', 'George Orwell', 'D', 1, '1984 é um romance distópico de George Orwell que introduz os leitores a uma sociedade totalitária onde o governo controla todos os aspectos da vida dos cidadãos.'),
+(1005, 'A Metamorfose', 'Franz Kafka', 'E', 1, 'A Metamorfose é uma novela escrita por Franz Kafka que explora a história de Gregor Samsa, um homem que se transforma em um inseto gigante, examinando as complexas relações familiares e sociais que isso desencadeia.');
+
