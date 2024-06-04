605223cc90d173cbc3dba2450e32dc36f3636de1
Iago F. <n3rdydzn@hotmail.com>
2024-06-04 20:32:38 -0300

    fix: emprestimos POST

--- api/emprestimos/post.php
+++ api/emprestimos/post.php
@@ -19,6 +19,31 @@ switch ($action) {
         $com .= $id_status;
         $com .= " WHERE id_livro=" . $id_livro . ";";
         break;
+	case 'registrar':
+        $verify = ["rm" => false,"id_bibliotecaria" => false,"id_livro" => false, "data_aluguel"=> false, "id_status_emprestimo" => false, "prazo" => false];
+        
+        $com = "insert into emprestimos (";
+        foreach (array_keys($_POST) as $key) {
+
+            if(array_key_exists($key, $verify)){
+                $verify[$key] = true;
+                $com .= $key . ",";
+            }
+            else{ echo(json_encode(["status" => "error", "message" => "um parâmetro inexistente foi enviado na requisição."])); die(); }
+        }
+        if (array_search(false, array_values($verify)) !== false) { echo(json_encode(["status" => "error", "message" => "um parâmetro obrigatório está faltando."])); die(); }
+
+
+        $com = substr_replace($com, "", -1) . ") values (";
+        foreach (array_values($_POST) as $value) {
+            $com .= "'" . $value . "',";
+        }
+        $com = substr_replace($com, "", -1) . ");";
+
+		echo($com);
+		die();
+
+		break;
 }
 
 try {
--- api/generos_livros/get.php
+++ api/generos_livros/get.php
@@ -8,6 +8,11 @@ $rs = "";
 //        break;
 //}
 
+if(empty($param)){
+	echo(json_encode(['status' => 'error', 'message'=>'Informe o campo ID']));
+	die();
+}
+
 $com = 'SELECT GLI.id_livro, GLI.id_genero, li.titulo, g.genero FROM `generos_livros` as GLI INNER JOIN generos as g INNER JOIN livros as li WHERE GLI.id_livro = li.id AND GLI.id_genero = g.id AND li.id='. $param . ';';
 
 
