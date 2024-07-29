669e7a47fd8136ec28c7fb59ca1a5bfd6dff7526
Iago Fragnan <n3rdydzn@hotmail.com>
2024-07-29 15:16:16 -0300

    fix: avaliacoes returning error 500

--- api/avaliacoes/get.php
+++ api/avaliacoes/get.php
@@ -2,17 +2,19 @@
 $com = "";
 $rs = "";
 
+
 switch ($action) {
     case 'media':
         if($param == null){
             echo(json_encode(["status" => "error", "message" => "Informe o id do livro para consultar a média."]));
             return;
         }
-        $rs = $db->prepare("SELECT COUNT(avaliacao) as avaliadores, AVG(avaliacao) as nota FROM avaliacoes WHERE id_livro = $param;");
+        $com = ("SELECT COUNT(avaliacao) as avaliadores, AVG(avaliacao) as nota FROM avaliacoes WHERE id_livro = $param;");
         break;
     case 'listar':
     default:
-        $rs = $db->prepare("SELECT av.id, al.nome,av.avaliacao FROM avaliacoes as av INNER JOIN alunos as al WHERE av.rm_aluno=al.rm;");
+        $com = ("SELECT * FROM avaliacoes as av INNER JOIN alunos as al where al.rm =av.rm_aluno;");
         break;
 }
+
 echo((new DB())->query($com));
