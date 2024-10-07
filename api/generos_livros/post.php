1d22c11fe79fc25b37716189f062a4f83777caae
Iago Fragnan <n3rdydzn@hotmail.com>
2024-10-07 14:37:59 -0300

    feat: verification generos_livros before adding value

--- api/generos_livros/post.php
+++ api/generos_livros/post.php
@@ -5,6 +5,22 @@ $message = "";
 
 switch ($action) {
     case 'inserir':
+
+        if($_POST['id_genero'] == 0 || $_POST['id_livro'] == 0 || !isset($_POST['id_livro']) || !isset($_POST['id_genero'])){
+            echo(json_encode(['status' => 'error', 'message' => 'Valor enviado inválido ou não correspondente']));
+            die();
+        }
+
+        //SELECT * FROM generos_livros WHERE id_livro=32 AND id_genero=3;
+        $verification = "SELECT * FROM generos_livros WHERE id_livro=" . $_POST['id_livro'] . ' AND id_genero=' .  $_POST['id_genero'] . ';';
+
+        $result = json_decode((new DB())->query($verification));
+        if($result->status == 'success'){
+            echo(json_encode(['status' => 'error', 'message' => 'Genero já está atribuido a este livro.']));
+            die();
+        }
+
+
         $com = "INSERT INTO generos_livros (";
         $values = "";
         foreach ($_POST as $key => $value) {
