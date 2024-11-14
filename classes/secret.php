5367cf633aa895868d8b3f323ca13a81c1050158
Iago Fragnan <n3rdydzn@hotmail.com>
2024-11-14 19:22:49 -0300

    remove: nothing to see #3 :(

--- classes/secret.php
+++ classes/secret.php
@@ -7,7 +7,7 @@ class config{
     "db_user" => "n3rdydev",
     "db_pass" => "N3rdygamerbr@123",
     "db_name" => "tccapi",
-    "imagesApiKey" => "e904bc960e7c33800f6de0c7cd9d1fbf1cbfa94a20459551c20a45c6ba52e830"
+    "imagesApiKey" => ""
   ];
 
   function get(){
